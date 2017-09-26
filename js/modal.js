8a8653b431d4ed9dc93ea5961ae1f322ffcf09af
Shreya <shreya@what.it.is>
2017-09-26 11:51:49 -0500

    wildenstein history/about page updates

--- js/modal.js
+++ js/modal.js
@@ -11,6 +11,7 @@
                     id: postId,
                 },
                 success: function(response) {
+                    
                     // parse json
                     response = JSON.parse(response);
 
@@ -20,37 +21,39 @@
                     if(response.type == 'success') {
                         // trigger the modal to open here however you want. this is just an example
                         // $('.main-modal').html(response.result);
+
+                        if(response.post.post_type == 'notableworks') {
+                            $('.main-modal.notable-works .notable-works-content').html('<div class="nw-modal-img-wrapper"><img class="nw-modal-img" src="' + response.image + '"></div>' + '<label>' + response.artist_name + '</label>' + ', ' + '<label class="main-label">' + response.title + '</label>'+ '<label>' + ', ' + response.supporting_details + '</label>');
+                        }
+
+                        if(response.post.post_type == 'artists') {
+                            // list out your variables from the response
+                            console.log('Nationality:', response.nationality);
+                            console.log('Bio:', response.bio);
+                            console.log('Artist Gallery:', response.artist_gallery);
+                            console.log('Artist Name:',response.artist_name);
+                            console.log('Page Title',response.page_title);
+                            console.log('Overview Text', response.artist_overview_text);
+                            console.log('Image', response.image);
+                            console.log('lable:', response.title);
+                            console.log('Publication Image',response.publication_image);
+                            console.log('Publications Title:',response.publication_title);
+                            console.log('Publications subtitle',response.publication_subtitle);
+                            console.log('Publications Brief', response.publication_brief);
+                            console.log('Publications Date', response.publication_date);
+                            console.log('No of Pages', response.no_of_pages);
+
+                            // add the data to the modal (just an example)
+                            $('.main-modal .artist-bio').html('<h3>' + response.artist_name + '</h3>' + '<label>' + response.nationality + '</label>' + '<label>' + response.date_of_birth + '</label>' + '<label>' + response.date_of_death + '</label>' + '<p class="small">' + response.bio + '</p>');
+
+                            response.artist_gallery.forEach(function(slide) {
+                                console.log('Slide Data:', slide);
+                                $('.main-modal .gallery-slider').append('<div><div class="bg-image" style="background-image: url('+ slide.image +')"></div>' + '<label class="main-label">' + slide.title + '</label></div>');
+                            }, this);
+                        }
                     
-                        // list out your variables from the response
-                        console.log('Nationality:', response.nationality);
-                        console.log('Bio:', response.bio);
-                        console.log('Artist Gallery:', response.artist_gallery);
-                        console.log('Artist Name:',response.artist_name);
-                        console.log('Page Title',response.page_title);
-                        console.log('Overview Text', response.artist_overview_text);
-                        console.log('Image', response.image);
-                        console.log('lable:', response.title);
-                        console.log('Publications Title:',response.publication_title);
-                        console.log('Publications subtitle',response.publication_subtitle);
-                        console.log('Publications Brief', response.publication_brief);
-                        console.log('Publications Date', response.publication_date);
-                        console.log('No of Pages', response.no_of_pages);
-
-
                         
-
-                        // add the data to the modal (just an example)
-                        $('.main-modal .artist-bio').html('<h3>' + response.artist_name + '</h3>' + '<label>' + response.nationality + '</label>' + '<label>' + response.date_of_birth + '</label>' + '<label>' + response.date_of_death + '</label>' + '<p class="small">' + response.bio + '</p>');
-                        
-                        $('.main-modal.notable-works .notable-works-content').html('<div class="bg-image" style="background-image: url('+ response.image +')"></div>' + '<label>' + response.artist_name + '</label>' + ', ' + '<label class="main-label">' + response.title + '</label>'+ '<label>' + ', ' + response.supporting_details + '</label>');
-                        
-                        $('.main-modal.publications .publications-content').html('<label class="main-label">' + response.publication_title + '</label>'+ '<p class="small">' + response.publication_subtitle, response.publication_brief + '</p>' + '<label class="main-label">' + response.publication_date , response.no_of_pages + '</label>');
-                        
-
-                        response.artist_gallery.forEach(function(slide) {
-                            console.log('Slide Data:', slide);
-                            $('.main-modal .gallery-slider').append('<div><div class="bg-image" style="background-image: url('+ slide.image +')"></div>' + '<label class="main-label">' + slide.title + '</label></div>');
-                        }, this);
+                        $('.main-modal .publications .publications-content').html('<div class="bg-image" style="background-image: url('+ response.publication_image +')"></div>' + '<h3>' + response.publication_title + '</h3>'+ '<label class="main-label">' + response.publication_subtitle + '</label>' + ', ' + '<label>'  + response.publication_brief + '</label>'+', '+'<label>'  + response.publication_date + '</label>'+ '<label>' + response.no_of_pages +'</label>');
                         
 
                     } else if(response.type == 'error') {
--- single-publications.php
+++ single-publications.php
@@ -8,17 +8,30 @@
  */
 
 get_header(); ?>
-<div id="primary" class="content-area">
+<div id="primary" class="content-area"> 
 		<div id="content" class="site-content" role="main">
 			<div class="main-modal">
 				<div class="container">
-						<div class="publications-content">
-                            <!-- <h3><?php the_field(''); ?></h3> -->
-                            
-							<label class="main-label"><?php the_field('publication_title'); ?></label>
-                            <p class="small"><?php the_field('publication_subtitle'); ?><?php the_field('publication_brief');?></p>
-                            <label class="main-label"><?php the_field('publication_date')?>,<?php the_field('no_of_pages');?></label>			
-						</div>
+						<section class="publications">
+                            <div class="row around-xs">
+                                
+                                <div class="col-xs-12 col-sm-5">
+                                    <div class="publications-content">
+                                        <label><?php the_field('publication_date');?></label>
+                                        <h3><?php the_field('publication_title'); ?></h3>
+                                        <label class="main-label"><?php the_field('publication_subtitle'); ?></label>
+                                        <label><?php the_field('publication_brief'); ?></label>
+                                        <label><?php the_field('no_of_pages'); ?></label>
+                                    </div>
+                                </div>
+                                <div class="col-xs-12 col-sm-5">
+                                    <div class="publications-image">
+                                        <div class="bg-image" style="background-image: url(<?php the_field('publication_image'); ?>)"></div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </section>
                 </div>
 			</div>
 		</div><!-- #content -->
