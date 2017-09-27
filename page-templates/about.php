1242b4843228ba3e4d1d2fe1b3b22a66fa7fff67
Jesse Kevon <Jessekevon@gmail.com>
2017-09-27 11:16:56 -0500

    standardized section blocks and removed about + history styles

--- page-templates/about.php
+++ page-templates/about.php
@@ -9,26 +9,27 @@
 
 get_header(); ?>
 	<main>
-		<div class="hero with-overlay bg-image with-icon" style="background-image: url('http://localhost:5132/wp-content/uploads/2017/09/bg.png')">
+		<div class="hero with-overlay bg-image with-icon" style="background-image: url('<?php the_field('cover_image'); ?>')">
 			<h1 class="white">About Us</h1>
-			<span class="down-arrow"></span>
+			<!-- <span class="down-arrow"></span> -->
 		</div>
 		<?php if(have_rows('content_blocks')):?>
 			<?php while(have_rows('content_blocks')): the_row(); ?>
-			<section class="background">
+			<section class="section-block">
 				<div class="container">
-					<div class="row center-xs">
-						<div class="col-xs-12 col-md-6 section-image"> 
+					<div class="row center-xs middle-xs">
+						<div class="col-xs-12 col-sm-4"> 
 							<div class="bg-image" style="background-image: url(<?php the_sub_field('section_image'); ?>)"></div>
 						</div>
-						<div class="col-xs-12 col-md-6 about-content"> 
-							<h3><?php (the_sub_field('subhead'));?></h3>
-							<h4><?php (the_sub_field('headline'));?></h4>
-							<p><?php (the_sub_field('body_copy'));?></p>
+						<div class="col-xs-12 col-sm-4 col-sm-offset-1"> 
+							<div class="text-block">
+								<!-- <h3><?//php (the_sub_field('subhead'));?></h3> -->
+								<h3><?php (the_sub_field('headline'));?></h3>
+								<p><?php (the_sub_field('body_copy'));?></p>
+							</div>
 						</div>
 					</div>
 				</div>
-				
 			</section>
 			<?php endwhile;?>
 		<?php endif;?>
--- page-templates/history.php
+++ page-templates/history.php
@@ -8,32 +8,31 @@
  */
 
 get_header(); ?>
-	<main>                 
-        <div class="hero with-overlay bg-image with-icon" style="background-image: url('http://localhost:5132/wp-content/uploads/2017/09/bg-history-no-overlay.png')">
-            <h1 class="white">Our History</h1>
-            <h4 class="white"><?php the_field('scroll_cta');?></h4> 
-            <span class="down-arrow"></span>
-        </div>
-
-        <section class="history-body">
+<main>
+    <div class="hero with-overlay bg-image with-icon" style="background-image: url('<?php the_field('cover_image'); ?>')">
+        <h1 class="white">Our History</h1>
+        <!-- <span class="down-arrow"></span> -->
+    </div>
+    <?php if(have_rows('content_blocks')):?>
+        <?php while(have_rows('content_blocks')): the_row(); ?>
+        <section class="section-block">
             <div class="container">
-                <?php if(have_rows('content_blocks')):?>
-                    <?php while(have_rows('content_blocks')): the_row(); ?>
-                        <div class="row center-xs">
-                            <div class="col-xs-12 col-md-6 section-image"> 
-                                <div class="bg-image" style="background-image: url(<?php the_sub_field('section_image'); ?>)"></div>
-                                <div></div> 
-                            </div>
-                            <div class="col-xs-12 col-md-6 content-block history-content"> 
-                                <h4><?php (the_sub_field('year'));?></h4>
-                                <h3><?php (the_sub_field('headline'));?></h3>
-                                <p><?php (the_sub_field('body_copy'));?></p>
-                            </div>
+                <div class="row center-xs">
+                    <div class="col-xs-12 col-sm-4"> 
+                        <img src="<?php the_sub_field('section_image'); ?>">
+                    </div>
+                    <div class="col-xs-12 col-sm-4 col-sm-offset-1"> 
+                        <div class="text-block">
+                            <span class="date"><?php (the_sub_field('year'));?></span>
+                            <h3><?php (the_sub_field('headline'));?></h3>
+                            <p><?php (the_sub_field('body_copy'));?></p>
                         </div>
-                    <?php endwhile;?>
-                <?php endif;?>
+                    </div>
+                </div>
             </div>
         </section>
-    </main>
+        <?php endwhile;?>
+    <?php endif;?>
+</main>
 <?php
 get_footer();
