228694434f72d3cc0bf4738e2c98ec8cde8103c0
Jesse Kevon <Jessekevon@gmail.com>
2017-09-28 08:56:01 -0500

    some mobile styles on homepage

--- page-templates/homepage.php
+++ page-templates/homepage.php
@@ -10,16 +10,14 @@
 get_header(); ?>
 	<main class="home">
 		<div class="hero split-screen">
-			<div class="hero-block">
+			<div class="hero-block" style="background-image: url(<?php the_field('hero_image'); ?>)"></div>
+
+			<div class="hero-block with-text">
 				<div class="inner">
 					<h3 class="white"><?php the_field('hero_headline');?></h3>
 					<p class="white"><?php the_field('intro_text');?></p>
-					<a class="btn rounded light" href="<?php echo get_field('hero_cta')['url'];?>"><?php echo get_field('hero_cta')['title'];?></a>
+					<a class="btn rounded light block-mobile" href="<?php echo get_field('hero_cta')['url'];?>"><?php echo get_field('hero_cta')['title'];?></a>
 				</div>
-			</div>
-
-			<div class="hero-block" style="background-image: url(<?php the_field('hero_image'); ?>)">
-
 			</div>
 		</div>
 
@@ -30,7 +28,7 @@ get_header(); ?>
 						<div class="col-xs-12 col-sm-3">
 							<aside>
 								<h4><?php the_field('artist_section_headline');?></h4>
-								<a class="btn rounded dark" href="<?php echo get_field('section_cta')['url'];?>"><?php echo get_field('section_cta')['title'];?></a>
+								<a class="btn rounded dark block-mobile desktop" href="<?php echo get_field('section_cta')['url'];?>"><?php echo get_field('section_cta')['title'];?></a>
 							</aside>
 						</div>
 
@@ -49,7 +47,7 @@ get_header(); ?>
 							<div class="row center-xs">
 								<?php foreach( $posts as $post): // variable must be called $post (IMPORTANT) ?>
 									<?php setup_postdata($post); ?>
-										<div class="col-xs-12 col-sm-3">
+										<div class="col-xs-6 col-sm-3">
 											<a href="<?php the_permalink(); ?>">
 												<div class="img-wrap">
 													<?php the_post_thumbnail(); ?>
@@ -62,6 +60,9 @@ get_header(); ?>
 						</div>
 						
 						<?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
+
+						<a class="btn rounded dark block-mobile mobile" href="<?php echo get_field('section_cta')['url'];?>"><?php echo get_field('section_cta')['title'];?></a>
+
 					<?php endif; ?>
 
 				</div>
@@ -89,14 +90,14 @@ get_header(); ?>
 						<aside>
 							<h4><?php the_field('section_headline');?></h4>
 							<p><?php the_field('section_copy');?></p>
-							<a class="btn rounded dark" href="<?php echo get_field('news_cta')['url'];?>"><?php echo get_field('news_cta')['title'];?></a>
+							<a class="btn rounded dark desktop" href="<?php echo get_field('news_cta')['url'];?>"><?php echo get_field('news_cta')['title'];?></a>
 						</aside>
 					</div>
 
 					<div class="col-xs-12 col-sm-8">
 						<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
 							<div class="row">
-								<div class="col-xs-12 col-sm-4">
+								<div class="col-xs-5 col-sm-4">
 									<a href="<?php the_permalink(); ?>">
 										<div class="fill">
 											<?php the_post_thumbnail('alm-cta'); ?>
@@ -104,12 +105,14 @@ get_header(); ?>
 									</a>
 								</div>
 
-								<div class="col-xs-12 col-sm-8">
+								<div class="col-xs-7 col-sm-8">
 									<div class="news-info">
 										<span class="date"><?php the_date('m.j.Y'); ?></span>
 										<a href="<?php the_permalink(); ?>"><h4><?php the_title(); ?></h4></a>
-										<p><?php the_excerpt(); ?></p>
-										<a class="read-more" href="<?php the_permalink(); ?>" class="with-arrow"><span class="chevron-arrow-right"></span>Read More</a>
+										<div class="desktop">
+											<p><?php the_excerpt(); ?></p>
+											<a class="read-more" href="<?php the_permalink(); ?>" class="with-arrow"><span class="chevron-arrow-right"></span>Read More</a>
+										</div>
 									</div>
 								</div>
 							</div>
@@ -117,6 +120,8 @@ get_header(); ?>
 
 					</div>
 				</div>
+				<?php wp_reset_postdata(); // IMPORTANT - resets the cfs object ?>
+				<a class="btn rounded dark block-mobile mobile" href="<?php echo get_field('news_cta')['url'];?>"><?php echo get_field('news_cta')['title'];?></a>
 			</section>
 		</div>
 	</main>
