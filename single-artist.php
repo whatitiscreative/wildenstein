dce3856c1e189a6f6612fd9a7448566683ffbed2
sf-derald <junie@what.it.is>
2017-09-29 17:49:29 -0500

    doesnt matter

--- single-artist.php
+++ single-artist.php
@@ -11,7 +11,7 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<div id="content" class="site-content" role="main">
-			<div class="main-modal">
+			<div class="">
 				<div class="container">
 					<section class="single-artist-content">
 						<?php
