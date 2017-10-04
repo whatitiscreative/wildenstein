f392a1b7192ec2a7c53a832b551bc6b192ce1b05
Jesse Kevon <Jessekevon@gmail.com>
2017-10-04 15:51:49 -0500

    ...

--- js/modal.js
+++ js/modal.js
@@ -29,7 +29,7 @@
                         }, 1500);
 
                         if(response.post.post_type == 'notableworks') {
-                            $('.main-modal.notable-works .notable-works-content').html('<div class="nw-modal-img-wrapper"><img class="nw-modal-img" src="' + response.image + '"></div>' + '<label>' + response.artist_name + '</label>' + ', ' + '<label class="main-label">' + response.title + '</label>'+ '<label>' + ', ' + response.supporting_details + '</label>');
+                            $('.main-modal.notable-works .notable-works-content').html('<div class="nw-modal-img-wrapper"><img class="nw-modal-img" src="' + response.image + '">' , '<label>' + response.artist_name + '</label>' + ', ' + '<label class="main-label">' + response.title + '</label>'+ '<label>' + ', ' + response.supporting_details + '</label>'</div>');
                         }
 
                         if(response.post.post_type == 'artist') {
